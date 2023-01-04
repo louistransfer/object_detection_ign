729ea8b14e7c78bf89298e7192c9e926cd07cb10
louistransfer <lberto60@gmail.com>
2023-01-04 17:14:15 +0100

    Linted test files

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,23 @@
 from starlite.status_codes import HTTP_200_OK, HTTP_201_CREATED
 from starlite.testing import TestClient
 
+
 def test_health_check(test_client: TestClient):
     with test_client as client:
         response = client.get("/health")
         assert response.status_code == HTTP_200_OK
         assert response.text == "healthy"
 
+
 def test_address(test_client: TestClient, address_data: dict):
     with test_client as client:
         response = client.post("/inference/address", json=address_data)
         assert response.status_code == HTTP_201_CREATED
-        assert type(response.content)==bytes
+        assert type(response.content) == bytes
+
 
 def test_location(test_client: TestClient, location_data: dict):
     with test_client as client:
         response = client.post("/inference/location", json=location_data)
         assert response.status_code == HTTP_201_CREATED
-        assert type(response.content)==bytes
+        assert type(response.content) == bytes
--- tests/test_wmts.py
+++ tests/test_wmts.py
@@ -1,5 +1,6 @@
 from object_detection_ign.wmts.satellite_view import WMTSClient
 
+
 def test_wmts_client(wmts_test_client: WMTSClient):
     assert len(wmts_test_client.available_options) > 0
     assert len(wmts_test_client.wmts_instance.contents) > 0
